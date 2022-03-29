b9fda88c3a4c28c722825b3e7bf73de46b403c89
Arie Blumenzweig <blumzi@013.net>
2022-03-29 11:33:41 +0300

    Cleanup + better WiFi connection

--- Shutter.ino
+++ Shutter.ino
@@ -8,29 +8,15 @@
 
 // LoLin NodeMCU V3, ESP-12E
 
-// https://www.posital.com/en/products/communication-interface/ssi/ssi-encoder.php
-
-static const uint8_t ssi_data_pin = D2;		// ssi data
-static const uint8_t ssi_clk_pin = D3;		// ssi clk
-static const uint8_t enc_preset_pin = D7;	// zeroes the encoder
-static const uint8_t led_pin = D5;			  // i'm alive LED
-static const uint8_t debug_pin = D6;		  // if grounded, debug to serial port
-
-static const uint8_t echo_pin = D2;       // HC-SR04 echo pin
-static const uint8_t trig_pin = D3;       // HC-SR04 trig pin
-static const uint8_t mode_pin = D8;       // selects between draw-wire encoder and ultra-sonic range finder
+static const uint8_t echo_pin       = D2; // HC-SR04 echo pin
+static const uint8_t trig_pin       = D3; // HC-SR04 trig pin
+static const uint8_t led_pin        = D5;	// i'm alive LED
+static const uint8_t debug_pin      = D6;	// if grounded, debug to serial port
 
 NewPing sonar(trig_pin, echo_pin, 400); // NewPing setup of pins and maximum distance.
 
-static const String version = "1.1";
+static const String version = "1.2";
 bool serialWasInitialized = false;
-
-#define POS_BITS		12
-#define POS_MASK		((1 << POS_BITS) - 1)
-#define MAX_POS			(1 << POS_BITS)
-#define TURN_BITS		12
-#define TURN_MASK		((1 << TURN_BITS) - 1)
-#define TOTAL_BITS		25
 
 #define DEBUG
 #ifdef DEBUG
@@ -43,12 +29,6 @@ bool serialWasInitialized = false;
 
 unsigned int lookAliveInterval = 5000;
 elapsedMillis timeFromLastlookAlive = 0;
-elapsedMillis timeFromStartZeroing = 0;
-elapsedMillis timeFromSonarRead = 0;
-bool zeroing = false;
-
-enum OpMode { WIRE = 0, SONAR = 1 };
-OpMode opMode = SONAR;
 
 WiFiServer server(80);
 
@@ -69,17 +49,8 @@ bool debugging() {
 
 void setup()
 {
-  pinMode(ssi_clk_pin, OUTPUT);
-  digitalWrite(ssi_clk_pin, HIGH);
-
-  pinMode(ssi_data_pin, INPUT);
-
   pinMode(led_pin, OUTPUT);
-  digitalWrite(led_pin, HIGH);
-
-  pinMode(enc_preset_pin, OUTPUT);
-  digitalWrite(enc_preset_pin, LOW);
-
+  digitalWrite(led_pin, HIGH);              // LED ON till WiFi is connected and server started
   pinMode(debug_pin, INPUT_PULLUP);
 
   debugln("\nWise40 Dome Shutter Server.");
@@ -88,7 +59,7 @@ void setup()
 
   server.begin();
   debugln("Server started.");
-  digitalWrite(led_pin, LOW);
+  digitalWrite(led_pin, LOW);               // LED OFF, we're in business
 }
 
 String enc_type(uint8_t t) {
@@ -98,114 +69,128 @@ String enc_type(uint8_t t) {
     case ENC_TYPE_CCMP:	return "CCMP(WPA)";
     case ENC_TYPE_NONE:	return "NONE";
     case ENC_TYPE_AUTO:	return "AUTO";
-    default:			return "Unknown";
+    default:			      return "Unknown";
   }
 }
 
 
 String connection_status(int status) {
   switch (status) {
-    case WL_NO_SHIELD:      return("No shield");
-    case WL_IDLE_STATUS:    return("Idle");
-    case WL_NO_SSID_AVAIL:  return("No SSID available");
-    case WL_SCAN_COMPLETED: return("Scan completed");
-    case WL_CONNECTED:      return("Connected");
-    case WL_CONNECT_FAILED: return("Connect failed");
-    case WL_CONNECTION_LOST:return("Connection lost");
-    case WL_WRONG_PASSWORD: return("Wrong password");
-    case WL_DISCONNECTED:   return("Disconnected");
+    case WL_NO_SHIELD:      return "No shield" ;
+    case WL_IDLE_STATUS:    return "Idle" ;
+    case WL_NO_SSID_AVAIL:  return "No SSID available" ;
+    case WL_SCAN_COMPLETED: return "Scan completed" ;
+    case WL_CONNECTED:      return "Connected" ;
+    case WL_CONNECT_FAILED: return "Connect failed" ;
+    case WL_CONNECTION_LOST:return "Connection lost" ;
+    case WL_WRONG_PASSWORD: return "Wrong password" ;
+    case WL_DISCONNECTED:   return "Disconnected" ;
     default:                return "Unknown";
   }
 }
+
 void connectWifi() {
-  int n, nDetected;
+  int i, nDetected;
+  String ssid, password;
 
-  nDetected = 0;
-  do {
-    debugln("Detecting networks ...");
-    WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
-    delay(500);
-    nDetected = WiFi.scanNetworks();
-    debug("Detected ");
-    debug(nDetected);
-    debugln(" networks");
-  } while (nDetected == 0);
+  while (WiFi.status() != WL_CONNECTED) {
+    
+    nDetected = 0;
+    do {
+      debugln("Detecting networks ...");
+      WiFi.mode(WIFI_STA);
+      WiFi.disconnect();
+      delay(500);
+      nDetected = WiFi.scanNetworks();
+      debug("Detected ");
+      debug(nDetected);
+      debugln(" networks");
+    } while (nDetected == 0);
 
-  String ssid;
-  String password = String("not-detected");
-
-  for (n = 0; n < nDetected; n++) {
-    ssid = WiFi.SSID(n);
-
-    debug("SSID: \"");
-    debug(ssid);
-    debug("\", RSSI: ");
-    debug(WiFi.RSSI(n));
-    debug(" dB, Enc: ");
-    debugln(enc_type(WiFi.encryptionType(n)));
-
-    if (ssid.compareTo("TheBlumz") == 0) {
-      password = String("gandalph1");
-      break;
-    } else if (ssid.compareTo("brutus") == 0) {
-      password = String("negev2008");
-      break;
-    } else if (ssid.compareTo("wo") == 0) {
-      password = String("");
-      break;
-    }
-  }
-
-  if (password.compareTo("not-detected") == 0) {
-    debugln("Did not detect a known WIFI network, bailing out!");
-    return;
-  }
-
-  int attempts, attempt;
-
-  for (attempts = 5, attempt = 0; attempt < attempts; attempt++) {
-
-    debug("\nAttempting to connect to \"");
-    debug(ssid);
-    debug("\", passwd: \"");
-    debug(password);
-    debug("\" attempt #");
-    debug(attempt);
-    debugln("");
-
-    if (ssid.compareTo("wo") == 0) {
-      IPAddress ip(192, 168, 1, 6);
-      IPAddress subnet(255, 255, 255, 0);
-      IPAddress gateway(192, 168, 1, 1);
-      IPAddress dns(132, 66, 65, 135);
-
-      WiFi.config(ip, dns, gateway, subnet);
-    }
-    WiFi.begin(ssid.c_str(), password.c_str());
-
-    elapsedMillis timeFromConnect = 0;
-    while ((WiFi.status() != WL_CONNECTED) && timeFromConnect < 20000) {
-      delay(200);
-      debug(".");
-    }
-
-    //if (WiFi.status() == WL_CONNECTED) {
-      debugln();
-      debug("Connection status: ");
-      debug(connection_status(WiFi.status()));
-      //debugln("");
-      debug(", Connected to \"");
+    password = String("not-detected");
+  
+    for (i = 0; i < nDetected; i++) {
+      ssid = WiFi.SSID(i);
+  
+      debug("SSID: \"");
       debug(ssid);
-      debug("\" as ");
-      debug(WiFi.localIP());
-      debug(", MAC: ");
-      debugln(WiFi.macAddress());
-      debugln();
-      break;
-    //}
-    //else
-      //WiFi.disconnect();
+      debug("\", RSSI: ");
+      debug(WiFi.RSSI(i));
+      debug(" dB, Enc: ");
+      debugln(enc_type(WiFi.encryptionType(i)));
+  
+      if (ssid.compareTo("TheBlumz") == 0) {
+        password = String("gandalph1");
+        break;
+      } else if (ssid.compareTo("brutus") == 0) {
+        password = String("negev2008");
+        break;
+      } else if (ssid.compareTo("wo") == 0) {
+        password = String("");
+        break;
+      }
+    }
+  
+    if (password.compareTo("not-detected") == 0) {
+      debugln("Did not detect a known WIFI network, re-scanning!");
+      continue;
+    }
+
+    int attempts, attempt;
+  
+    for (attempts = 5, attempt = 0; attempt < attempts; attempt++) {
+  
+      debug("\nAttempting to connect to \"");
+      debug(ssid);
+      debug("\", passwd: \"");
+      debug(password);
+      debug("\" attempt #");
+      debug(attempt);
+      debugln("");
+  
+      if (ssid.compareTo("wo") == 0) {
+        IPAddress ip(192, 168, 1, 6);
+        IPAddress subnet(255, 255, 255, 0);
+        IPAddress gateway(192, 168, 1, 1);
+        IPAddress dns(132, 66, 65, 135);
+  
+        WiFi.config(ip, dns, gateway, subnet);
+      }
+      WiFi.begin(ssid.c_str(), password.c_str());   // start connecting to the known network
+  
+      elapsedMillis timeFromConnect = 0;
+      while ((WiFi.status() != WL_CONNECTED) && timeFromConnect < 30000) {    // try for 30 seconds to connect to the known network
+        delay(200);
+        debug(".");
+      }
+  
+      if (WiFi.status() == WL_CONNECTED) {
+        debugln();
+        debug("WiFi status: ");
+        debug(connection_status(WiFi.status()));
+        debug(", Connected to \"");
+        debug(ssid);
+        debug("\" as ");
+        debug(WiFi.localIP());
+        debug(", MAC: ");
+        debugln(WiFi.macAddress());
+        debugln();
+        return;
+      }
+      else
+      {
+        debugln();
+        debug("After ");
+        debug(timeFromConnect);
+        debug(" millis, WiFi status: ");
+        debug(connection_status(WiFi.status()));
+        debug(" . Giving up on SSID \"");
+        debug(ssid);
+        debugln("\"");
+        WiFi.disconnect();
+        delay(100);
+      }
+    }
   }
 }
 
@@ -245,94 +230,13 @@ void lookAlive() {
 unsigned int read_range_cm() {
   unsigned int cm = sonar.convert_cm(sonar.ping_median(10));
 
-  debug("sonar: "); debug(cm); debugln(" cm");
+  debug("sonar range: "); debug(cm); debug(" cm");
+  debug(", Wifi: "); debugln(connection_status(WiFi.status()));
+  
   return cm;
 }
 
-int ssi_read_bit() {
-  digitalWrite(ssi_clk_pin, LOW);
-  delayMicroseconds(6);
-  digitalWrite(ssi_clk_pin, HIGH);
-  delayMicroseconds(5);
-  return digitalRead(ssi_data_pin);
-}
-
-unsigned long ssi_read_single() {
-  int i, bit;
-  unsigned long value = 0;
-
-  for (i = 0; i < TOTAL_BITS; i++) {	// pump-out bits
-    bit = ssi_read_bit();
-    value |= bit;
-    value <<= 1;
-  }
-  return value;
-}
-
-void debug_single(unsigned long value) {
-  String *s = new String();
-
-  for (int i = TOTAL_BITS - 1; i >= 0; i--) {
-    *s += (value & (1 << i)) ? "1" : "0";
-  }
-  debugln(*s);
-  delete s;
-}
-
-//
-// Performs an encoder multi-transmission (see https://www.posital.com/en/products/communication-interface/ssi/ssi-encoder.php).
-// The value is read twice, with a single clock cycle in-between.  If the two values do not match, they are thrown away.
-//
-String ssi_read_encoder() {
-  unsigned long value[2] = { 0, 0 },  v;
-  int turns, pos;
-
-  do {
-    value[0] = ssi_read_single();				// read first value
-    digitalWrite(ssi_clk_pin, LOW);
-    delayMicroseconds(5);
-    digitalWrite(ssi_clk_pin, HIGH);
-    delayMicroseconds(5);
-    value[1] = ssi_read_single();				// read second value
-
-    if (value[0] != value[1]) {
-      debug("mismatch: ");
-      debug(value[0]);
-      debug(" != ");
-      debugln(value[1]);
-    } else
-      debug_single(value[0]);
-
-  } while (value[0] != value[1]);
-
-  digitalWrite(ssi_clk_pin, HIGH);
-  delayMicroseconds(25);
-
-  pos = value[0] & POS_MASK;
-  turns = (value[0] >> 13) & TURN_MASK;
-
-  v = (turns * MAX_POS) + pos;
-  debug("turns: ");
-  debug(turns);
-  debug(", pos: ");
-  debug(pos);
-  debug(" => ");
-  debugln(v);
-  return String(v);
-}
-
-
 String help() {
-  if (opMode == WIRE)
-    return String("<table>"
-                  " <tr><th align='left'>Cmd</th><th align='left'>Arg</th><th align='left'>Desc</th></tr>"
-                  " <tr><td>help</td><td/><td>shows this help<td></tr>"
-                  " <tr><td>encoder</td><td/><td>gets the current encoder value</td></tr>"
-                  " <tr><td>status</td><td/><td>prints \"ok\", if alive</td></tr>"
-                  " <tr><td>version</td><td/><td>prints the software version</td></tr>"
-                  " <tr><td>zero</td><td>?password=******</td><td>zeroes the encoder</td></tr>"
-                  "</table>");
-  else
     return String("<table>"
                   " <tr><th align='left'>Cmd</th><th align='left'>Arg</th><th align='left'>Desc</th></tr>"
                   " <tr><td>help</td><td/><td>shows this help<td></tr>"
@@ -353,17 +257,8 @@ String make_http_reply(String req) {
   if (req.indexOf("GET /status HTTP/1.1") != -1) {
     content = String("ok");
   }
-  else if (opMode == WIRE && req.indexOf("GET /encoder HTTP/1.1") != -1) {
-    content = ssi_read_encoder();
-  }
-  else if (opMode == SONAR && req.indexOf("GET /range HTTP/1.1") != -1) {
+  else if (req.indexOf("GET /range HTTP/1.1") != -1) {
     content = String(read_range_cm());
-  }
-  else if (opMode == WIRE && req.indexOf("GET /zero?password=ne%27Gev HTTP/1.1") != -1) {
-    zeroing = true;
-    digitalWrite(enc_preset_pin, HIGH);
-    timeFromStartZeroing = 0;
-    content = String("encoder zeroed");
   }
   else if (req.indexOf("GET /help HTTP/1.1") != -1) {
     content = help();
@@ -398,19 +293,7 @@ void loop()
 {
   lookAlive();
   
-  if (opMode == SONAR)
-    read_range_cm();
-
-  if (opMode == WIRE && zeroing && timeFromStartZeroing >= 120) {
-    //
-    // From: https://www.posital.com/media/en/fraba/productfinder/posital/datasheet-ixarc-ocd-sx_1.pdf
-    //
-    // The encoder value will be set to 0 after the preset input was active
-    // for 100 ms and changes to inactive again
-    //
-    digitalWrite(enc_preset_pin, LOW);
-    zeroing = false;
-  }
+  read_range_cm();    // for debugging
 
   WiFiClient client = server.available();
   if (client)
@@ -430,7 +313,7 @@ void loop()
         String line = client.readStringUntil('\r');
         request += line;
 
-        // wait for end of client's request, that is marked with an empty line
+        // wait for end of client's request, marked by an empty line
         if (line.length() == 1 && line[0] == '\n')
         {
           client.println(make_http_reply(request));
@@ -438,7 +321,7 @@ void loop()
         }
       }
     }
-    delay(1); // give the web browser time to receive the data
+    delay(1000); // give the web browser time to receive the data
 
     // close the connection:
     client.stop();
